82d171c42a62bc2545194c60ea5d5e6017248bda
Jonas56 <youneslamkhanter02@gmail.com>
2020-06-17 13:34:50 +0200

    Updates

---
+++ precache-manifest.294353a504eb4546ad9502a10448742e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd1bf56fcc3f268ee74a64954798e6d9",
+    "url": "/robotfreundes/index.html"
+  },
+  {
+    "revision": "837e74b6be092c04b8f0",
+    "url": "/robotfreundes/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "b56d4127b2609ca3a1cf",
+    "url": "/robotfreundes/static/css/main.5f95fb79.chunk.css"
+  },
+  {
+    "revision": "837e74b6be092c04b8f0",
+    "url": "/robotfreundes/static/js/2.b42a18e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robotfreundes/static/js/2.b42a18e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b56d4127b2609ca3a1cf",
+    "url": "/robotfreundes/static/js/main.7e6319bd.chunk.js"
+  },
+  {
+    "revision": "c33f756e43657309a59e",
+    "url": "/robotfreundes/static/js/runtime-main.d99b74b2.js"
+  }
+]);
